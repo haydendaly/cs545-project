5374bbbad923535673785b553fec241db260f452
joncucci <62709104+joncucci@users.noreply.github.com>
2020-11-22 12:38:30 -0500

    Update Business.jsx

--- src/Pages/Business.jsx
+++ src/Pages/Business.jsx
@@ -1,10 +1,16 @@
+import { getSuggestedQuery } from "@testing-library/react";
 import React from "react";
 
 const Business = props => {
     const { business } = props;
     return (
-        <div style={{ height: 200, width: 500, backgroundColor: "red", marginBottom: 10 }}>
-            <p>{business.name}</p>
+        <div style={{height: 300, width: 1000, backgroundColor: "lightgrey", marginTop: 100, marginLeft: 400}}>
+            <h1 style = {{marginLeft: 325}}>{business.name}</h1>
+            <img src= {business.img} alt = {business.name} align = "left"></img>
+            <p style = {{marginLeft: 325}}>{business.stars}...................{business.miles} Stars</p>
+            <p style = {{marginLeft: 325}}>{business.indoor} {business.outdoor} {business.delivery} {business.temperature} Capacity : {business.capacity} persons</p>
+            <br></br><br></br><br></br>
+            <p style = {{marginLeft: 325}}>{business.description}</p>
         </div>
     )
 }
